42249c31aa0a58e0d6240021dd4b13ecdc965aaf
Nick Dease <56933343+ndease@users.noreply.github.com>
2021-12-10 16:43:56 -0500

    add script to pull alert message from libguides

---
+++ js/alert.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', (event) => {
+  var alertLength = document.querySelector("div.lib-site-alert p").textContent.length;
+
+    if (alertLength > 10){
+    document.querySelector("#s-lg-widget-1639149482290").style.width="auto";
+    document.querySelector("#s-lg-widget-1639149482290").style.display="inline-block";
+    document.querySelector(".lib-site-alert").style.display = "block";
+    }
+    else{
+      document.querySelector(".lib-site-alert").style.display = "none";
+    }
+});
